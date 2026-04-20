937888d4a913d3f02971fc39c9eab8f570b2aad3
afternoons.46@gmail.com <afternoons.46@gmail.com>
2026-04-20 15:26:42 +0700

    Day 13-Template

---
+++ resources/views/landing/components/css.blade.php
@@ -0,0 +1,29 @@
+<!-- Latest Bootstrap min CSS -->
+	<link rel="stylesheet" href="{{ asset('/storage/landing/asset/bootstrap/css/bootstrap.min.css') }}">
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css2?family=Exo:wght@300;400;500;600;700;800;900&display=swap"
+		rel="stylesheet">
+	<!-- Font Awesome CSS -->
+	<link rel="stylesheet" href="{{ asset('/storage/landing/asset/fonts/font-awesome.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('/storage/landing/asset/fonts/themify-icons.css') }}">
+	<!--- owl carousel Css-->
+	<link rel="stylesheet" href="{{ asset('/storage/landing/asset/owlcarousel/css/owl.carousel.css') }}">
+	<link rel="stylesheet" href="{{ asset('/storage/landing/asset/owlcarousel/css/owl.theme.css') }}">
+	<!--fonts icons Css-->
+	<link rel="stylesheet" href="{{ asset('/storage/landing/asset/css/fonts.css') }}">
+	<!--prettyPhoto css-->
+	<link href="{{ asset('/storage/landing/asset/css/prettyPhoto.css') }}" rel="stylesheet">
+	<!-- animate CSS -->
+	<link rel="stylesheet" href="{{ asset('/storage/landing/asset/css/animate.css') }}">
+	<!-- Slick css -->
+	<link rel="stylesheet" href="{{ asset('/storage/landing/asset/css/slick.css') }}">
+	<!-- Style CSS -->
+	<link rel="stylesheet" href="{{ asset('/storage/landing/asset/css/menu.css') }}">
+	<link rel="stylesheet" href="{{ asset('/storage/landing/asset/css/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('/storage/landing/asset/css/responsive.css') }}">
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
---
+++ resources/views/landing/components/js.blade.php
@@ -0,0 +1,27 @@
+<!-- Latest jQuery -->
+	<script src="{{ asset('/storage/landing/asset/js/jquery-1.12.4.min.js') }}"></script>
+	<!-- Latest compiled and minified Bootstrap -->
+	<script src="{{ asset('/storage/landing/asset/bootstrap/js/bootstrap.min.js') }}"></script>
+	<!-- modernizer JS -->
+	<script src="{{ asset('/storage/landing/asset/js/modernizr-2.8.3.min.js') }}"></script>
+	<!-- stellar js -->
+	<script src="{{ asset('/storage/landing/asset/js/jquery.stellar.min.js') }}"></script>
+	<!-- Menu js -->
+	<script src="{{ asset('/storage/landing/asset/js/menu.js') }}"></script>
+	<script src="{{ asset('/storage/landing/asset/js/jquery.sticky.js') }}"></script>
+	<!-- owl-carousel min js  -->
+	<script src="{{ asset('/storage/landing/asset/owlcarousel/js/owl.carousel.min.js') }}"></script>
+	<!-- MAGNIFICANT JS -->
+	<script src="{{ asset('/storage/landing/asset/js/jquery.magnific-popup.min.js') }}"></script>
+	<!-- Slick JS -->
+	<script src="{{ asset('/storage/landing/asset/js/slick.min.js') }}"></script>
+	<!-- jquery mixitup min js -->
+	<script src="{{ asset('/storage/landing/asset/js/jquery.mixitup.js') }}"></script>
+	<!-- jquery.prettyPhoto js -->
+	<script src="{{ asset('/storage/landing/asset/js/jquery.prettyPhoto.js') }}"></script>
+	<!-- scrolltopcontrol js -->
+	<script src="{{ asset('/storage/landing/asset/js/scrolltopcontrol.js') }}"></script>
+	<!-- WOW - Reveal Animations When You Scroll -->
+	<script src="{{ asset('/storage/landing/asset/js/wow.min.js') }}"></script>
+	<!-- scripts js -->
+	<script src="{{ asset('/storage/landing/asset/js/scripts.js') }}"></script>
